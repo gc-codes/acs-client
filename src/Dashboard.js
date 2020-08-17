ba2d7e95b1be31ac7a0fee958564852907b6f905
Kushan Chatterjee <kushan@megdap.com>
2020-08-17 22:49:49 +0530

    Redesigned chat and calling UI

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Navbar, Jumbotron, Button, Card, FormControl, InputGroup } from 'react-bootstrap';
+import { Navbar, Jumbotron, Button, Card, FormControl, InputGroup, Media, Container, Row, Col } from 'react-bootstrap';
 import logo from './logo.svg';
+import { ChatFeed, Message } from 'react-chat-ui';
 import { CallingFactory, UserAccessTokenCredential, CallOptions } from '@skype/spool-sdk';
 import { createClientLogger, setLogLevel } from '@azure/logger';
 import CallCard from './CallCard';
@@ -19,7 +20,14 @@ class Dashboard extends Component {
             selectedMicrophoneDeviceId: null,
             showCameraNotFoundWarning: false,
             showSpeakerNotFoundWarning: false,
-            showMicrophoneNotFoundWarning: false
+            showMicrophoneNotFoundWarning: false,
+            messages: [
+                new Message({
+                    id: 1,
+                    message: "Hey, how you doing?",
+                }),
+                new Message({ id: 0, message: "Hey, I'm doing great" }),
+            ],
         };
     }
 
@@ -115,7 +123,7 @@ class Dashboard extends Component {
 
     render() {
         return (
-            <div>
+            <div style={{ height: '80%' }}>
                 <Navbar bg="dark" variant="dark">
                     <Navbar.Brand href="#home">
                         <img
@@ -127,8 +135,95 @@ class Dashboard extends Component {
                         />{' '}
                         Azure Communication Services
                     </Navbar.Brand>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            <b>Identity:</b> { this.userDetails.identity }
+                        </Navbar.Text>
+                    </Navbar.Collapse>
                 </Navbar>
-                <Jumbotron>
+                <Container style={{ height: '100%', marginTop: '40px' }}>
+                    <Row style={{ height: '100%' }}>
+                        <Col style={{ padding: '0px' }}>
+                            <Card style={{ borderRadius: '0px', height: '100%' }}>
+                                <Card.Title style={{ paddingLeft: '14px', paddingTop: '12px' }}>Users</Card.Title>
+                                <ul className="list-unstyled">
+                                    <li><Card.Header><img src="https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-friends-matt-leblanc.jpg" style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '8px' }} />Joey Tribbiani</Card.Header></li>
+                                    <li><Card.Header><img src="https://pbs.twimg.com/profile_images/928266336073117696/W2gCidjA_400x400.jpg" style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '8px' }}/>Jake Peralta</Card.Header></li>
+                                    <li><Card.Header><img src="https://pyxis.nymag.com/v1/imgs/079/792/3ed0d94be0a9bd3d023f00532889bab152-30-chandler-bing.rsquare.w330.jpg" style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '8px' }}/>Chandler Bing</Card.Header></li>
+                                    <li><Card.Header><img src="https://i.pinimg.com/236x/72/e2/a5/72e2a574c0b19066ffda716eceeb7d78.jpg" style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '8px' }}/>Amy Santiago</Card.Header></li>
+                                </ul>
+                            </Card>
+                        </Col>
+                        <Col style={{ padding: '0px' }}>
+                            <Card style={{ width: '700px', borderRadius: '0px', height: '100%' }}>
+                                <Card.Header>
+                                    <b>Joey Tribbiani</b>
+                                    <Button style={{ float: 'right', borderTopLeftRadius: '0px', borderBottomLeftRadius: '0px' }} id="placeCall-btn" onClick={() => this.placeCall()}><i className="fas fa-phone"></i></Button>
+                                    <FormControl
+                                        style={{ width: '200px', float: 'right', borderTopRightRadius: '0px', borderBottomRightRadius: '0px' }}
+                                        placeholder="Enter destination user ID"
+                                        aria-describedby="placeCall-btn"
+                                        id="destinationID"
+                                        onChange={this.handleDestinationIdChange}
+                                    />
+
+                                </Card.Header>
+                                <Card.Body>
+                                    {/* Calling UI goes here */}
+                                    {
+                                        this.state.call && (<CallCard call={this.state.call}
+                                            callClient={this.state.callClient}
+                                            selectedCameraDeviceId={this.state.selectedCameraDeviceId}
+                                            selectedSpeakerDeviceId={this.state.selectedSpeakerDeviceId}
+                                            selectedMicrophoneDeviceId={this.state.selectedMicrophoneDeviceId} />)
+                                    }
+                                    {/* Messages go here */}
+                                    {!this.state.call &&
+                                        (<ChatFeed
+                                            messages={this.state.messages} // Boolean: list of message objects
+                                            isTyping={this.state.is_typing} // Boolean: is the recipient typing
+                                            hasInputField={false} // Boolean: use our input, or use your own
+                                            showSenderName // show the name of the user who sent the message
+                                            bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
+                                            // JSON: Custom bubble styles
+                                            bubbleStyles={
+                                                {
+                                                    text: {
+                                                        fontSize: 14
+                                                    },
+                                                    chatbubble: {
+                                                        borderRadius: 4,
+                                                        padding: 8
+                                                    }
+                                                }
+                                            }
+                                        />)}
+                                </Card.Body>
+                                <Card.Footer>
+                                    <InputGroup className="mb-3" style={{ width: '100%' }}>
+                                        <FormControl
+                                            placeholder="Type a message"
+                                            aria-describedby="sendMessage-btn"
+                                            onChange={this.handleMessageChange}
+                                        />
+                                        <InputGroup.Append>
+                                            <Button variant="success" id="sendMessage-btn" onClick={() => { }}>Send</Button>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
+
+// Old Components
+{/* <Jumbotron>
                     <h1>Hello, John Doe!</h1>
                     <p>
                         <b>Identity:</b> {this.userDetails.identity} <br />
@@ -146,9 +241,9 @@ class Dashboard extends Component {
                             </InputGroup.Append>
                         </InputGroup>
                     </p>
-                </Jumbotron>
-                {/* Call Card */}
-                <Card style={{ width: '65%', marginTop: '40px', marginLeft: '24px' }}>
+                </Jumbotron> */}
+{/* Call Card */ }
+{/* <Card style={{ width: '65%', marginTop: '40px', marginLeft: '24px' }}>
                     <Card.Body>
                         <Card.Title>Call section</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
@@ -162,12 +257,6 @@ class Dashboard extends Component {
                             }
                         </Card.Body>
                     </Card.Body>
-                </Card>
-                {/* Chat Component */}
-                <Chat />
-            </div>
-        );
-    }
-}
-
-export default Dashboard;
+                </Card> */}
+{/* Chat Component */ }
+{/* <Chat /> */ }
